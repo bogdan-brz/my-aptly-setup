ab8b5e5d8cab99c85d058027ea038075e53e4d04
BoggyB21 <bogdan.brzuchacz@gmail.com>
2022-09-14 19:13:50 -0700

    try 2

--- pages/api/download.js
+++ pages/api/download.js
@@ -7,38 +7,69 @@ import {
     sendFileForDownload,
     zipUp,
 } from "../../utils/fsHelpers";
+import path from "path";
 
 const handler = async (req, res) => {
     const { setupid, separate, setupid1, setupid2 } = req.query;
     const rand = Math.floor(Math.random() * 1000);
+    console.log("herererere");
     if (separate) {
         if (setupid1 == null || setupid2 == null) return res.redirect("/");
         const frontPath = findFolderDir(setupid1);
         const backPath = findFolderDir(setupid2);
-        await makeDir(`./package_upload/prep_area/${rand}/YourSetup`);
+        console.log("here");
+        console.log(
+            path.join(
+                process.cwd(),
+                `package_upload/prep_area/${rand}/YourSetup`
+            )
+        );
+        await makeDir(
+            path.join(
+                process.cwd(),
+                `package_upload/prep_area/${rand}/YourSetup`
+            )
+        );
         await copyDir(
             frontPath,
-            `./package_upload/prep_area/${rand}/YourSetup/client`
+            path.join(
+                process.cwd()`package_upload/prep_area/${rand}/YourSetup/client`
+            )
         );
         await copyDir(
             backPath,
-            `./package_upload/prep_area/${rand}/YourSetup/server`
+            path.join(
+                process.cwd()`package_upload/prep_area/${rand}/YourSetup/server`
+            )
         );
         await zipUp(
-            `./package_upload/prep_area/${rand}/YourSetup`,
-            `./package_upload/prep_area/${rand}/frontandback.zip`
+            path.join(
+                process.cwd()`package_upload/prep_area/${rand}/YourSetup`
+            ),
+            path.join(
+                process.cwd()`package_upload/prep_area/${rand}/frontandback.zip`
+            )
         );
         await sendFileForDownload(
-            `./package_upload/prep_area/${rand}/frontandback.zip`,
+            path.join(
+                process.cwd()`package_upload/prep_area/${rand}/frontandback.zip`
+            ),
             res,
             async () => {
-                await cleanupDir(`./package_upload/prep_area/${rand}`);
+                await cleanupDir(
+                    path.join(process.cwd()`package_upload/prep_area/${rand}`)
+                );
             }
         );
     } else {
         if (setupid == null) return res.redirect("/");
         const packagePath = findPackageDir(setupid);
-        await sendFileForDownload(packagePath, res, () => {});
+        console.log(path.join(process.cwd(), packagePath));
+        await sendFileForDownload(
+            path.join(process.cwd(), packagePath),
+            res,
+            () => {}
+        );
     }
 };
 
@@ -46,20 +77,20 @@ export default handler;
 
 // TO CREATE NEW ARCHIVE
 // await copyDir(
-//     "./package_upload/constructs/front/react/blank_react",
-//     "./package_upload/prep_area/setup1"
+//     "package_upload/constructs/front/react/blank_react",
+//     "package_upload/prep_area/setup1"
 // );
 // await zipUp(
-//     "./package_upload/prep_area/setup1",
-//     "./package_upload/prep_area/setup1.zip"
+//     "package_upload/prep_area/setup1",
+//     "package_upload/prep_area/setup1.zip"
 // );
 // await sendFileForDownload(
-//     "./package_upload/ready_packages/front/blank_react.zip",
+//     "package_upload/ready_packages/front/blank_react.zip",
 //     res,
 //     async () => {
-//         await cleanupDir("./package_upload/prep_area/setup1");
+//         await cleanupDir("package_upload/prep_area/setup1");
 //         await cleanupFile(
-//             "./package_upload/prep_area/setup1.zip"
+//             "package_upload/prep_area/setup1.zip"
 //         );
 //     }
 // );
